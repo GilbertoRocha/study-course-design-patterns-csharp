a773ae6e404d6d71204372ed962ec41d3d3cf311
Giba <giba.darocha@gmail.com>
2025-10-22 07:27:27 -0300

    Added Single Responsability Principle

---
+++ srp/Utils/Client.cs
@@ -0,0 +1,27 @@
+﻿namespace srp;
+
+public class Client
+{
+	public Client() {}
+
+	public void CreateClient()
+	{
+
+	}
+
+	public void ReadClient() 
+	{
+
+	}
+
+	public void UpdateClient()
+	{
+		Console.WriteLine("Client updated");
+	}
+
+	public void DeleteClient() 
+	{
+
+	}
+
+}
---
+++ srp/Utils/Notify.cs
@@ -0,0 +1,21 @@
+﻿namespace srp;
+
+public class Notify
+{
+
+	private readonly Client _client;
+	
+	public Notify(Client client)
+	{
+		_client = client;
+	}
+
+	
+	public void NotifyClient()
+	{
+		Console.WriteLine("Notifying the client");
+		_client.UpdateClient();
+
+	}
+
+}
